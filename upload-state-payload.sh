4a924f406a2a77aa9a22997c34497c474774c8f0
Joshua Tracy <joshua.tracy@hashicorp.com>
2022-04-14 09:33:17 -0400

    upload the state payload to new workspace

---
+++ upload-state-payload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Did you set the TFC_TOKEN envioronment variable yet? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+read -p "Enter the URL of the TFE/TFC instance. Example: tfe.mydomain.com: " URL
+read -p "Enter ther WORKSPACE ID: " WORKSPACE_ID
+
+HTTP_RESPONSE=$(curl \
+--header "Authorization: Bearer "$TFC_TOKEN"" \
+--header "Content-Type: application/vnd.api+json" \
+--request POST \
+--data @payload.json \
+https://$URL/api/v2/workspaces/$WORKSPACE_ID/state-versions)
+
+echo $HTTP_RESPONSE
+
+
