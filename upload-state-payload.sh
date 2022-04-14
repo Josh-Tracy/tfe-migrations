5f6eb187365628b3ec9dd81093a272a0b2ca05ec
Joshua Tracy <joshua.tracy@hashicorp.com>
2022-04-14 10:25:46 -0400

    user token added

--- upload-state-payload.sh
+++ upload-state-payload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-read -p "Did you set the TFC_TOKEN envioronment variable yet? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "[INFO]--- A USER API TOKEN MUST BE USED TO UPLOAD STATE TO A LOCKED WORKSPACE, NOT AN ORG API TPKEN ---[INFO]"
+read -p "Did you set the TFC_USER_TOKEN envioronment variable yet? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 read -p "Enter the URL of the TFE/TFC instance. Example: tfe.mydomain.com: " URL
 read -p "Enter ther WORKSPACE ID: " WORKSPACE_ID
 
 HTTP_RESPONSE=$(curl \
---header "Authorization: Bearer "$TFC_TOKEN"" \
+--header "Authorization: Bearer "$TFC_USER_TOKEN"" \
 --header "Content-Type: application/vnd.api+json" \
 --request POST \
 --data @payload.json \
